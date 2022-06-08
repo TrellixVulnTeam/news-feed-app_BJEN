0419bc0df4d1b60c58d65736f21ab2084d32057d
MaxOF <egorovteam2019@yandex.ru>
2022-06-08 14:05:16 +0300

    add component and styles

--- src/features/Card.tsx
+++ src/features/Card.tsx
@@ -3,20 +3,133 @@ import './Cards.scss';
 
 export const Card = () => {
     return (
-        <div className='cardWrapper'>
-            <div className='imageWrap'>
-                <div className='image'>
-                    1
+        <div className={"block"}>
+            <div className={"block__row"}>
+                <div className={"block__element block__element_1"}>
+                    <div className={"container__card"}>
+                        <div className="block__image">
+                            <div className={"image"}>
+                                1
+                            </div>
+                        </div>
+                        <div className="block__content">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                            По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                        </div>
+                        <div className="block__topic">
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className='textField'>
-                <span className='textHeader'>
-                    По стандарту РБК заголовок материала должен содержать не более 73 знаков
-                </span>
-                По стандарту РБК заголовок материала должен содержать не более 73 знаков
-            </div>
-            <div className='topic'>
-                Топик
+                <div className="block__element block__element_2">
+                    <div className="wrapperCards">
+                        <div className={"container__card2"}>
+                            <div className="block__image2">
+                                <div className={"image2"}>
+                                    2
+                                </div>
+                            </div>
+                            <div className="block__content2">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                        <div className={"container__card3"}>
+                            <div className="block__image3">
+                                <div className={"image3"}>
+                                    3
+                                </div>
+                            </div>
+                            <div className="block__content3">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="block__element block__element_3">
+                    <div className="wrapperCards3">
+                        <div className={"container__card4"}>
+                            <div className="block__image4">
+                                <div className={"image4"}>
+                                    4
+                                </div>
+                            </div>
+                            <div className="block__content4">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                        <div className={"container__card5"}>
+                            <div className="block__image5">
+                                <div className={"image5"}>
+                                    5
+                                </div>
+                            </div>
+                            <div className="block__content5">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                        <div className={"container__card6"}>
+                            <div className="block__image6">
+                                <div className={"image6"}>
+                                    6
+                                </div>
+                            </div>
+                            <div className="block__content6">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                        <div className={"container__card7"}>
+                            <div className="block__image7">
+                                <div className={"image7"}>
+                                    7
+                                </div>
+                            </div>
+                            <div className="block__content7">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+                        <div className={"container__card8"}>
+                            <div className="block__image8">
+                                <div className={"image8"}>
+                                    8
+                                </div>
+                            </div>
+                            <div className="block__content8">
+                            <span style={{fontWeight: 'bold', display: 'block'}}>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                            </span>
+                                По стандарту РБК заголовок материала должен содержать не более 73 знаков
+                                <pre style={{margin: 0}}>Топик</pre>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     );
